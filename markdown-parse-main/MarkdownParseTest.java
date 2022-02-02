91e0dbdb242593332addc8bb76bfabcf95f01ae8
skyler <sjgoh@ucsd.edu>
2022-02-02 15:13:45 -0800

    updates

--- markdown-parse-main/MarkdownParseTest.java
+++ markdown-parse-main/MarkdownParseTest.java
@@ -1,9 +1,9 @@
 import static org.junit.Assert.*;
+import org.junit.*;
 import java.util.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
